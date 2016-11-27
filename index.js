f0906aa6ebe8f3291d1df29cc816898ea8ab11fd
Alexandr Subbotin <kelionweb@gmail.com>
2016-11-27 20:06:37 +0100

    Initial commit
    Added simple with package.json and and ability to fetch from external API and display results

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const emojiPlugin = ({term, display}) => {
+  const match = term.match(/^emoji?\s(.+)/);
+  if (match) {
+    const searchTerm = match[1];
+    const q = encodeURIComponent(searchTerm);
+    fetch(`http://emoji.getdango.com/api/emoji?q=${q}`)
+      .then(response => response.json())
+      .then(data => {
+        const items = data.results.map(x => ({
+          title: x.text
+        }));
+
+        display(items);
+      });
+  }
+};
+
+module.exports = {
+  fn: emojiPlugin
+}
